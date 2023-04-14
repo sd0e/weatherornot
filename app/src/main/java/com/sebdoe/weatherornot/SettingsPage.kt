388eca37dbd2d0057a6d29556d9b3e68497c2c34
sd0e <sebastiandoe5@gmail.com>
2023-04-14 21:23:13 +0100

    Add material3 and an assist chip

--- app/src/main/java/com/sebdoe/weatherornot/SettingsPage.kt
+++ app/src/main/java/com/sebdoe/weatherornot/SettingsPage.kt
@@ -1,10 +1,30 @@
 package com.sebdoe.weatherornot
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsPage(dataManager: DataManager) {
-    Dropdown(options = dataManager.locationIDs, selectedText = dataManager.location, onSelectedChange = { loc -> dataManager.location = loc; dataManager.refresh() })
+    Column() {
+        Dropdown(options = dataManager.locationIDs, selectedText = dataManager.location, onSelectedChange = { loc -> dataManager.location = loc; dataManager.refresh() })
+        AssistChip(
+            onClick = { dataManager.refresh() },
+            label = { androidx.compose.material3.Text(text = "Refresh") },
+            leadingIcon = {
+                Icon(Icons.Filled.Refresh, contentDescription = "Refresh the weather", modifier = Modifier.size(AssistChipDefaults.IconSize))
+            }
+        )
+    }
 }
