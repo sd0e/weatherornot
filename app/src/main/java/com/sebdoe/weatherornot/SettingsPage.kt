c0b1f3e55adb2f670d5656a9ac7bcfcfc9b7ee25
sebastiandoe5 <sebastiandoe5@gmail.com>
2023-04-15 15:03:07 +0100

    Update colour theming

--- app/src/main/java/com/sebdoe/weatherornot/SettingsPage.kt
+++ app/src/main/java/com/sebdoe/weatherornot/SettingsPage.kt
@@ -6,10 +6,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/com/sebdoe/weatherornot/ui/theme/Theme.kt
+++ app/src/main/java/com/sebdoe/weatherornot/ui/theme/Theme.kt
@@ -4,17 +4,17 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = darkColorScheme(
     primary = Purple200,
-    primaryVariant = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = lightColorScheme(
     primary = Purple500,
-    primaryVariant = Purple700,
     secondary = Teal200
 
     /* Other default colors to override
@@ -35,10 +35,16 @@ fun WeatherOrNotTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
         LightColorPalette
     }
 
-    MaterialTheme(
+    /*MaterialTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
+        content = content
+    )*/
+
+    androidx.compose.material3.MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
         content = content
     )
 }
